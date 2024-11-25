64b6ad2bcf39cdf13090bdaf7106991c574f8cb9
csmaxi <csmaxinro@gmail.com>
2024-11-25 17:35:28 -0300

    blank

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 function Footer() {
     return ( 
         <div>
-            <Link href="https://www.instagram.com/csmaxi/">
-            <small className="flex justify-center font-light  mb-6 text-lg">csmaxi 2024 - Contacto :  <CiInstagram className="h-8 w-8"/></small>
+            <Link href="https://www.instagram.com/csmaxi/" target="_blank">
+            <small className="flex justify-center font-light  mb-6 text-lg">csmaxi 2024 - Contacto :  <CiInstagram className="h-8 w-8 font-bold"/></small>
             
             </Link>
         </div>
